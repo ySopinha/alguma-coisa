3abd183e72e48aecfb332442c9a8f6050d0fd5c6
so <sofiacataovc@gmail.com>
2024-09-24 10:23:41 -0300

    Add files via upload
    calculadora de tabuada!

---
+++ rasc.py
@@ -0,0 +1,20 @@
+tab = int(input("insira numero 1-10: "))
+for vezes in range(11):
+    print (f'({tab} x {vezes} = {tab * vezes})')
+
+'''
+Opção alernativa:
+
+numero = int(input('Digite um número: \f'))
+for i in range(11):
+    print(f'{numero} x {i} = {numero*i})
+
+
+TERMINAL:
+Número escolhido: 5
+
+5 x 0 = 0
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15 ...
+'''

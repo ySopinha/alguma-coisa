79631cc8091ea6aa191e993467d299ea1aec8da3
so <sofiacataovc@gmail.com>
2024-09-27 10:33:44 -0300

    Update tchiriritcharara.py

--- tchiriritcharara.py
+++ tchiriritcharara.py
@@ -1,1 +1,15 @@
-print("salsicha no molho")
+saldo = 1000
+def saldo(x):
+    global saldo
+    saldo -= x
+def solicitar_valor(x):
+    float(input('insira valor; '))
+def verificar_saldo(x):
+    global saldo
+    if x > saldo:
+        print('negado')
+
+valor = solicitar_valor()
+if verificar_saldo(valor):
+    saldo(valor)
+print (saldo)

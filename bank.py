3506d0632e6769006cc446e3b6d950fbed3f1b71
so <sofiacataovc@gmail.com>
2024-10-07 10:55:00 -0300

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,12 +1,63 @@
 saldo = 1000
 opcao = 0
-cliente = ['fulano' , 'fulana' , 'fulaninho', 'fulaninha', 'pingo', 'fifi']
 
 
-dicionario = [
-    {'nome': 'lilica legal uau querida', 'idade': '16', 'genero': 'feminino'},
-    {'nome': 'Mr. Cowboy Ratinho Fofinho', 'idade': '2', 'genero': 'ratinho eu acho'}
-]
+dicionario = {
+     '07171' : {'nome' : 'lilica queridinha uau fofa', 'usuario' : 'lilica', 'senha' : '6969' },
+     '07133' : {'nome' : 'oliver queridinho uau silly', 'usuario' : 'sosofi', 'senha' : '420420' },    
+     '07144' : {'nome' : 'pingo adora guardaroupa', 'usuario' : 'lilica', 'senha' : '6969' },
+}
+
+
+
+while opcao == 0:
+    def cadastro():
+            cpf = int(input('insira cpf: '))
+            dicionario.get(cpf)
+            nominho = input('Insira nome: ')
+            senhanha = input('insira senha 4 a 6 : ')
+            lista = nominho.split()
+            usuario = lista[0]
+            print(usuario)
+            print(f'Seu usuário é {usuario}, use-o no log-on.')
+            
+
+    def add_cliente():
+            saber = input('qual seu nome? ')
+            if saber in dicionario:
+                        print (f'legal, {saber}, você é um cliente!')
+            else:
+                print ('voce nao eh um cliente, voce precisa efetuar o cadastro.')
+                print(dicionario)
+    def remover_cliente():
+            know = input('insira nome')
+            if know in dicionario:
+
+                print('ta, foi removido')
+                print(dicionario)
+                
+            else:
+                print('nome nao ta na lista, daora')
+    def login():
+            log = input('insira aqui o seu login: ')
+            if log in dicionario:
+                senha_cliente = input('insira aqui a sua senha: ')
+            else:
+                print ('o usuário não foi encontado, tente novamente')
+    while opcao == 0:
+        print ('olá, como gostaria de acessar o banco insira nome do banco?')
+        print ('[0] cadastro')
+        print ('[1]log-on')
+        print ('[2]encerrar')
+        opt = int(input())
+        opcao = 1
+    if opt == 0:
+        cadastro()
+
+opcao = 1
+
+     
+
 
 
 print ('digite o numero')
@@ -22,29 +73,29 @@ numero = int(input())
 while opcao == 0:
     def cadastro():
         adic = input('insira nome')
-        cliente.append(adic)
-        print (cliente)
-
+        dicionario.append(adic)
+        print (dicionario)
+     
     def add_cliente():
         saber = input('qual seu nome? ')
-        if saber in cliente:
+        if saber in dicionario:
                     print (f'legal, {saber}, você é um cliente!')
         else:
             print ('voce nao eh um cliente, voce precisa efetuar o cadastro.')
-            print(cliente)
+            print(dicionario)
     def remover_cliente():
         know = input('insira nome')
-        if know in cliente:
-            cliente.remove(know)
+        if know in dicionario:
+
             print('ta, foi removido')
-            print(cliente)
+            print(dicionario)
             
         else:
             print('nome nao ta na lista, daora')
     match numero:
         case 1:
             adic = input('insira nome')
-            if adic in cliente:
+            if adic in dicionario:
                saque = int(input('de quanto seria o saque?'))
             if saque > saldo:
                 print ('você não tem saldo o suficiente.')
